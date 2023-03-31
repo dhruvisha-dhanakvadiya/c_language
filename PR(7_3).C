b12bdc992827ed9a81f9ec5fc26e816443480b53
dhruvisha-dhanakvadiya <128052033+dhruvisha-dhanakvadiya@users.noreply.github.com>
2023-03-31 18:44:18 +0530

    projects

---
+++ PR(7_3).C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int num,rev=0,a,b,rem,sum=0,copy;
+ clrscr();
+ printf("enter the number:");
+ scanf("%d",&num);
+ copy=num;
+ while(num!=0)
+ {
+   rem=num%10;
+   rev=rev*10+rem;
+   num=num/10;
+   sum=sum+rem;
+ }
+ if(rev*sum==copy)
+ {
+  printf("this is magic number");
+  }
+  else
+  {
+   printf("this is not magic number");
+   }
+   getch();
+  }
