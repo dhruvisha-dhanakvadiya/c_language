de520bd223befa6439b513bf93ca5e0cb5999b3d
dhruvisha-dhanakvadiya <128052033+dhruvisha-dhanakvadiya@users.noreply.github.com>
2023-04-06 09:19:56 +0530

    looping with pattern

---
+++ 7_12.C
@@ -0,0 +1,23 @@
+/*55555
+  4444
+  333
+  22
+  1*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   int i,j;
+   clrscr();
+   for(i=5;i>=1;i--)
+   {
+    for(j=1;j<=i;j++)
+    {
+     printf("%d",i);
+      }
+      printf("\n");
+      {
+      getch();
+   }
+}
+}
